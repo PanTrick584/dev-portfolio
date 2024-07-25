c59575e4ca8da98e18fe9c5b8e32b5966b311643
Patryk <chodacki.pc@gmail.com>
2024-07-25 23:38:06 +0200

    fixed scrolling down and up

--- src/containers/StickySectionImages.tsx
+++ src/containers/StickySectionImages.tsx
@@ -11,32 +11,50 @@ const colors: ContentItem[] = [
     { color: 'saddlebrown' },
     { color: 'hotpink' },
     { color: 'yellowgreen' },
-    { color: 'yellowgreen' },
+    // { color: 'yellowgreen' },
 ];
 
 const StickySectionImages: React.FC = () => {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const sectionCarouselRef = useRef<HTMLDivElement | null>(null);
     const [slideIn, setSlideIn] = useState<string>('slide-in');
+    const [style, setStyle] = useState<React.CSSProperties>({
+        backgroundColor: colors[0].color
+    });
 
     useEffect(() => {
         const handleScroll = () => {
-            setSlideIn('')
             if (sectionCarouselRef.current) {
                 const sectionTop = sectionCarouselRef.current.getBoundingClientRect().top;
                 const sectionHeight = sectionCarouselRef.current.clientHeight;
-                const scrollRatio = Math.min(Math.max(-sectionTop / sectionHeight, 0), 2);
+                const scrollRatio = Math.min(Math.max(-sectionTop / sectionHeight, 0), 1);
                 const newIndex = Math.min(Math.floor(scrollRatio * colors.length), colors.length -1);
                 setCurrentIndex(prev => {
                     // let direction = newIndex < prev ? "slide-in" : "slide-out";
+                    // if (prev === 0 || prev === colors.length-1) setSlideIn("slide-in");
+                    if (newIndex > prev) {
+                        // setSlideIn("slide-in");
+                        setSlideIn("")
+                        // setSlideIn("slide-in")
+                        setTimeout(() => setSlideIn("slide-in"), 100)
+                        setTimeout(() => setStyle({
+                            backgroundColor: colors[newIndex].color
+                        }), 100)
+
+                    }
+                    if (newIndex < prev) {
+                        setSlideIn("");
+                        setTimeout(() => setSlideIn("slide-in"), 100)
+                        setTimeout(() => setStyle({
+                            backgroundColor: colors[newIndex].color
+                        }), 100)
+                    }
+                    
+
                     if (newIndex !== prev) {
-                        console.log("rózne");
-                        // direction = 'slide-out';
                         return newIndex
                     }
-                    console.log(prev);
                     
-                    if (newIndex !== colors.length-1) setSlideIn("slide-in")
                     return prev;
                 });
             }
@@ -50,11 +68,13 @@ const StickySectionImages: React.FC = () => {
     return (
         <div
             className={`sticky-section-carousel`}
-            style={{height: `${colors.length}00vh`}}
+            style={{height: `${colors.length * 2}00vh`}}
             ref={sectionCarouselRef}
         >
             <div className="content" >
-                <div className={`content-box ${slideIn}`} style={{backgroundColor: `${colors[currentIndex].color}`}}>
+                <div
+                    className={`content-box ${slideIn}`}
+                    style={style}>
                     <p>HELLO!!!!</p>
                 </div>
             </div>
